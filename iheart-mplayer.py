85cbd027341ec4735c599a568a88f130820363cf
oldlaptop <oldlaptop654@aol.com>
2014-03-10 20:21:04 -0400

    Refactoring/restructuring
     The two drivers are coalesced into one, you choose between mplayer
     and vlc with a commandline option. driver_common is history, its
     functionality is now all either back in the unified driver or in
     parse_iheart_json. A few minor changes applied along the way -
     for instance, now standardized on double quotes for UI strings
     and single quotes for all other strings.

--- iheart-mplayer.py
+++ iheart-mplayer.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 import parse_iheart_json
-import driver_common
+import argparse
 import subprocess
 
 def launch_mplayer (url, mplayer_args):
@@ -16,23 +16,63 @@ def launch_mplayer (url, mplayer_args):
 	# mplayer will spend minutes looking for a nonexistent video stream.
 	subprocess.call (['mplayer', url, '-novideo'], shell=False)
 
+def launch_vlc (url, vlc_args):
+	# Make sure we actually have vlc
+	try:
+		subprocess.check_call (['which', 'vlc'], shell=False)
+	except subprocess.CalledProcessError:
+		raise RuntimeError ('VLC could not be found.')
+
+	# Now we run vlc.
+	subprocess.call (['vlc', url] + vlc_args, shell=False)
+
 if __name__ == '__main__':
-	parser = driver_common.build_parser ('Play an iheartradio station in MPlayer')
+	parser = argparse.ArgumentParser (description="Play an iHeartRadio station in mplayer or VLC")
+	parser.add_argument (
+		'stream_id',
+		nargs=1,
+		type=int,
+		help="The (five-digit?) ID number of the station",
+		)
+	parser.add_argument (
+		'-p', '--player',
+		default='mplayer',
+		choices=['mplayer', 'vlc'],
+		help="The player to use (currently either mplayer or vlc), the default is mplayer",
+		)
+	parser.add_argument (
+		'-o', '--player_options',
+		nargs='*',
+		help="Command-line arguments to pass the media player",
+		)
+	parser.add_argument (
+		'-t', '--stream_type',
+		default='auto',
+		help="The type of stream to use, currently either auto or one of shout, rtmp, stw, or pls. The default is auto.",
+		)
+	parser.add_argument (
+		'-v', '--verbose',
+		action='count',
+		help="Display extra information",
+		)
+
 	args = parser.parse_args ()
 
 	station = parse_iheart_json.station_info (args.stream_id[0])
 
 	if (args.verbose):
 		try:
-			driver_common.print_station_info (station)
+			print ("station name:", station['name'])
+			print ("description:", station['description'])
+			print ("broadcast format:", station['format'])
 		except KeyError:
 			print ("warning: a field is missing")
 			if (verbose >= 2):
 				print ("full dictionary dump:")
-				print ("station")
+				print (station)
 
 	try:
-		station_url = driver_common.get_station_url (station, args.stream_type)
+		station_url = parse_iheart_json.get_station_url (station, args.stream_type)
 	except KeyError:
 		print ("error: requested stream does not exist for this station")
 		if (args.verbose is not None and args.verbose >= 2):
@@ -40,4 +80,7 @@ if __name__ == '__main__':
 			print (station)
 		exit ()
 
-	launch_mplayer (station_url, [])
+	if (args.player == 'mplayer'):
+		launch_mplayer (station_url, [])
+	elif (args.player == 'vlc'):
+		launch_vlc (station_url, [])
--- parse_iheart_json.py
+++ parse_iheart_json.py
@@ -4,28 +4,6 @@ import urllib.request
 import urllib.parse
 import json
 
-# The iheartradio API is not publicly documented, to my knowledge. At the time
-# of writing, one can submit a POST request with a content of '1' to a URL of
-# the following form (where STATION_ID_NUMBER is a five-digit number):
-#
-# http://iheart.com/a/live/station/STATION_ID_NUMBER/stream/
-#
-# The response will be UTF-8 encoded JSON describing some vital information
-# about the station (if it exists), such as name, market, genre, and links to
-# various live streams of its content (often RTMP or HTTP, but usually (never?)
-# both). Valid station ID numbers can currently be obtained by searching for
-# stations on the http://iheart.com website - for example, in the following
-# URL, the station ID number is 1165:
-#
-# http://www.iheart.com/live/WOOD-Radio-1069-FM-1300AM-1165/
-
-# The base URL for our API request
-iheart_base_url = 'http://www.iheart.com/a/live/station/'
-
-# The postfix for our API request URL (comes after the ID number)
-iheart_url_postfix = 'stream/'
-
-
 def station_info (station_id):
 	''' Returns a dict containing all available information about station_id
 
@@ -33,6 +11,27 @@ def station_info (station_id):
 	No publicly documented method of obtaining a list of valid station_ids
 	is currently available.
 	'''
+
+	# The iheartradio API is not publicly documented, to my knowledge. At the time
+	# of writing, one can submit a POST request with a content of '1' to a URL of
+	# the following form (where STATION_ID_NUMBER is a five-digit number):
+	#
+	# http://iheart.com/a/live/station/STATION_ID_NUMBER/stream/
+	#
+	# The response will be UTF-8 encoded JSON describing some vital information
+	# about the station (if it exists), such as name, market, genre, and links to
+	# various live streams of its content (often RTMP or HTTP, but usually (never?)
+	# both). Valid station ID numbers can currently be obtained by searching for
+	# stations on the http://iheart.com website - for example, in the following
+	# URL, the station ID number is 1165:
+	#
+	# http://www.iheart.com/live/WOOD-Radio-1069-FM-1300AM-1165/
+
+	# The base URL for our API request
+	iheart_base_url = 'http://www.iheart.com/a/live/station/'
+
+	# The postfix for our API request URL (comes after the ID number)
+	iheart_url_postfix = 'stream/'
 
 	# We can't do this in one function call, since urljoin can't deal with
 	# more than two URL components.
@@ -51,3 +50,108 @@ def station_info (station_id):
 		raise RuntimeError(station['error'])
 	else:
 		return station
+
+def detect_stream (station):
+	'''Takes a station dictionary and determines the best stream URL to use,
+	returns its URL.
+	'''
+
+	# There are two keys within the station dictionary which contain dicts
+	# of stream URLs. The first is 'stream_urls', which seems to always have
+	# at least two members - 'rtmp' (which is an RTMP URL which VLC and some
+	# mplayer versions don't seem to understand) and 'http' (which is an
+	# HTTP URL which neither mplayer or vlc seem to understand in any case I
+	# have tested). Quite often, at least one of these will be None (but
+	# both are always present for a valid station). For our purposes we will
+	# ignore this member. The second is 'streams', which *only* contains
+	# members for those streams which actually exist (there will never be a
+	# None in this dict). Keys which have been observed:
+	#
+	# shoutcast_stream: Seems to always be a standard shoutcast stream URL.
+	#                   Widely understood and works in both VLC and mplayer,
+	#                   as far as I've tested.
+	#
+	# secure_rtmp_stream: An RTMP URL, not always the same as the RTMP URL
+	#                     in stream_urls. In cases I've seen it's more
+	#                     likely to work than the RTMP URL in stream_urls,
+	#                     but some mplayers still don't like it (VLC seems
+	#                     fine).
+	#
+	# stw_stream: Some kind of special HTTP-based stream, which neither
+	#             mplayer or VLC understand. Seems related to the (former?)
+	#             StreamTheWorld Flash-based platform, always(?) seems to
+	#             occur together with pls_stream.
+	#
+	# pls_stream: Contains a link to a PLS file (INI-based playlist). Occurs
+	#             alongside stw_stream. Seems to contain a large number of
+	#             HTTP links, none of which are the same as stw_stream.
+	#             Seems to work in VLC, but not any mplayer I have tested.
+	#
+	# Stations tend to have either both of the first two, OR both of the
+	# second two. There may be others I have not encountered - dictionary
+	# dumps (use -vv) of stations with other stream types would be greatly
+	# appreciated.
+
+
+	# For our purposes, the preference order is:
+	# shoutcast_stream
+	# pls_stream
+	# secure_rtmp_stream
+	# stw_stream
+	#
+	# stw_stream will print a warning, since it is not known to work in any
+	# player.
+	preference_order = ['shoutcast_stream',
+	                    'pls_stream',
+	                    'secure_rtmp_stream',
+	                    'stw_stream']
+
+	stream_dict = station['streams']
+
+
+	for candidate in preference_order:
+		try:
+			choice = stream_dict[candidate]
+			print ("stream type auto: using " + candidate)
+			break
+		except KeyError:
+			pass
+	else:
+		# we have an stw_stream - almost certain to not work
+		print ("warning: using stw_stream, this stream type is not known to work anywhere")
+	return choice
+
+def get_station_url (station, stream_type):
+	'''Takes a station dictionary and a stream type, and returns a URL.
+	Caller should be prepared for possible KeyErrors, since iheartradio
+	completely omits non-existent streams instead of setting their values
+	to null/None. If the stream type is 'auto', a stream will be detected
+	automatically using detect_stream().
+	'''
+
+	station_url = None
+
+	# If a stream does not exist, iheart seems to just omit its entry from
+	# the 'streams' category in JSON. Therefore we will bail out with a
+	# KeyError if the requested stream does not exist.
+	if (stream_type == 'auto'):
+		station_url = detect_stream (station)
+	elif (stream_type == 'rtmp'):
+		if (station['streams']['secure_rtmp_stream']):
+			station_url = station['streams']['secure_rtmp_stream']
+	elif (stream_type == 'shout'):
+		if (station['streams']['shoutcast_stream']):
+			station_url = station['streams']['shoutcast_stream']
+	elif (stream_type == 'stw'):
+		if (station['streams']['stw_stream']):
+			print ("warning: using stw_stream, this stream type is not known to work anywhere")
+			station_url = station['streams']['stw_stream']
+	elif (stream_type == 'pls'):
+		if (station['streams']['pls_stream']):
+			station_url = station['streams']['pls_stream']
+
+	# Apparently we don't usually get here, see above.
+	if (not station_url):
+		raise RuntimeError ("Requested stream does not exist")
+
+	return station_url
